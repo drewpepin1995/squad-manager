d864365d44ad0f8bcae4265379fa5ca2f801ac34
Rose <50706764+Edestiny7@users.noreply.github.com>
2019-12-02 21:07:28 -0500

    App.js now shows modules added to it & calendar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Schedule from './components/Schedule/schedule.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: {}
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="row justify-content-center">
+          <div className="col-12">
+            <Schedule />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Schedule/schedule.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+
+class Schedule extends Component {
+    state = {
+        date: new Date(),
+    }
+
+    onChange = date => this.setState({ date })
+
+    render() {
+        return (
+            <div>
+                <Calendar
+                    onChange={this.onChange}
+                    value={this.state.date}
+                />
+            </div>
+        );
+    }
+}
+export default Schedule;
