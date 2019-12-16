b3f4b5b70d0e666989b5f3710ad6171528617330
Drew Pepin <drewpepin1995@gmail.com>
2019-12-15 21:43:13 -0500

    adding underline in team summary card

--- src/components/info/TeamSummary.js
+++ src/components/info/TeamSummary.js
@@ -8,6 +8,7 @@ const TeamSummary =({team}) => {
                 <div className="card z-depth-0 project-summary" id="card">
                     <div className='card-content grey-text text-darken-3'>
                         <span className="orange-text card-title center">{team.name}</span>
+                        <hr></hr>
                         <p className="grey-text center">Team Manager : {team.managerFirstName}  {team.managerLastName}</p>
                         <p className="grey-text center">Sport : {team.sport}</p>
                     </div>
